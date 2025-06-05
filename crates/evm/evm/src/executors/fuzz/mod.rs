346c257e0c7fcd4497578f7467dca007bd589ca9
Emmanuel Antony <emmanuelantony2000@gmail.com>
2025-06-05 18:12:31 +0530

    Fixed the file cursor error

--- crates/evm/evm/src/executors/fuzz/mod.rs
+++ crates/evm/evm/src/executors/fuzz/mod.rs
@@ -29,7 +29,7 @@ use serde_json::json;
 use std::{
     cell::RefCell,
     collections::BTreeMap,
-    io::{Read, Write},
+    io::{Read, Seek, SeekFrom, Write},
     time::{Instant, SystemTime, UNIX_EPOCH},
 };
 
@@ -426,6 +426,8 @@ impl FuzzedExecutor {
                 let mut existing: Vec<File> = serde_json::from_slice(&existing).unwrap_or_default();
                 existing.push(result);
                 let result = serde_json::to_vec_pretty(&existing).unwrap();
+                file.seek(SeekFrom::Start(0)).unwrap();
+                file.set_len(0).unwrap();
                 file.write_all(&result).unwrap();
             }
 
